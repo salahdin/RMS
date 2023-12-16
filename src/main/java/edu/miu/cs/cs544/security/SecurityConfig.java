68020e7279348aec41a348667ffd2983a8842683
dhiman <dsarker@miu.edu>
2023-12-16 11:15:04 -0600

    add security config

---
+++ src/main/java/edu/miu/cs/cs544/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package edu.miu.cs.cs544.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails admin = User.withUsername("admin")
+                .password(encoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails client = User.withUsername("client")
+                .password(encoder.encode("client"))
+                .roles("CLIENT")
+                .build();
+        return new InMemoryUserDetailsManager(admin, client);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeRequests()
+                .requestMatchers("/users/ping").permitAll()
+                .and()
+                .authorizeRequests()
+                .requestMatchers("/users/**").authenticated()
+                .and().build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
