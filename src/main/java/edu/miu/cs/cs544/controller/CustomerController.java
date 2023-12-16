2ce53ef75add320ead0326330e919240735280ae
anvn <anvu.sg@gmail.com>
2023-12-16 09:03:17 -0600

    Add file for customer service

---
+++ src/main/java/edu/miu/cs/cs544/controller/CustomerController.java
@@ -0,0 +1,23 @@
+package edu.miu.cs.cs544.controller;
+
+import edu.miu.cs.cs544.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/customers")
+@RequiredArgsConstructor
+public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<?> addCustomer(@Valid  ){
+
+    }
+
+}
---
+++ src/main/java/edu/miu/cs/cs544/service/CustomerService.java
@@ -0,0 +1,8 @@
+package edu.miu.cs.cs544.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+}
