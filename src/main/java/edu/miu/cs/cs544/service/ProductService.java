f4be427c4198034969815fefeacd7bd2d4ed9391
Mikedicpro <77217801+Mikedicpro@users.noreply.github.com>
2023-12-17 14:30:40 -0600

    I added prodcutController...

---
+++ src/main/java/edu/miu/cs/cs544/service/ProductService.java
@@ -0,0 +1,35 @@
+package edu.miu.cs.cs544.service;
+
+import edu.miu.cs.cs544.domain.Product;
+import edu.miu.cs.cs544.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Integer id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public void deleteProduct(Integer id) {
+        productRepository.deleteById(id);
+    }
+}
