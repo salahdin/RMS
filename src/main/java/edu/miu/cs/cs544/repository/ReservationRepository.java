95db7cc37693dc23e0b0de63581e05b5879b5003
salah <salahdinjemal@gmail.com>
2023-12-18 16:23:27 -0600

    fix edge conditions

--- src/main/java/edu/miu/cs/cs544/repository/ReservationRepository.java
+++ src/main/java/edu/miu/cs/cs544/repository/ReservationRepository.java
@@ -16,6 +16,11 @@ public interface ReservationRepository extends JpaRepository<Reservation, Intege
     @Query("SELECT r FROM Reservation r JOIN r.items i WHERE i.product.id = :productId AND :date BETWEEN i.checkinDate AND i.checkoutDate")
     List<Reservation> findReservationsByProductAndDate(@Param("productId") Integer productId, @Param("date") LocalDate date);
 
-    @Query("SELECT r FROM Reservation r JOIN r.items i WHERE i.product.id = :productId AND (i.checkinDate <= :checkin AND i.checkoutDate >= :checkout OR i.checkinDate BETWEEN :checkin AND :checkout OR i.checkoutDate BETWEEN :checkin AND :checkout)")
+    @Query("SELECT r FROM Reservation r " +
+            "JOIN r.items i " +
+            "WHERE i.product.id = :productId " +
+            "AND ((i.checkinDate > :checkin AND i.checkoutDate < :checkout) " +
+            "OR (i.checkinDate BETWEEN :checkin AND :checkout) " +
+            "OR (i.checkoutDate BETWEEN :checkin AND :checkout))")
     List<Reservation> findReservationsByProductAndDates(@Param("productId") Integer productId, @Param("checkin") LocalDate checkin, @Param("checkout") LocalDate checkout);
 }
